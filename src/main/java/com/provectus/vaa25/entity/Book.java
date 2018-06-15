72e74f204846a85609106a095e0187cc1c426f1c
vaa25 <vaa25@yandex.ru>
2018-06-15 15:35:17 +0300

    remove redundant JsonProperty from Book.java

--- src/main/java/com/provectus/vaa25/entity/Book.java
+++ src/main/java/com/provectus/vaa25/entity/Book.java
@@ -1,6 +1,5 @@
 package com.provectus.vaa25.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -23,25 +22,19 @@ public final class Book {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
-    @JsonProperty
     private Long id;
-    @JsonProperty
     private String title;
     @OneToMany(targetEntity = Author.class, cascade = CascadeType.ALL)
     @JoinTable(name = "book_authors",
         joinColumns = @JoinColumn(name = "book_id"),
         inverseJoinColumns = @JoinColumn(name = "author_id"))
-    @JsonProperty
     private List<Author> authors;
     @OneToMany(targetEntity = Genre.class, cascade = CascadeType.ALL)
     @JoinTable(name = "book_genres",
         joinColumns = @JoinColumn(name = "book_id"),
         inverseJoinColumns = @JoinColumn(name = "genre_id"))
-    @JsonProperty
     private List<Genre>genres;
-    @JsonProperty
     private String description;
-    @JsonProperty
     private Double price;
 
     public Book(final Long id) {
