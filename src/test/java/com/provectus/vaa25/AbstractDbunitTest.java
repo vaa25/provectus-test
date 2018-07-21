dc2be98ed09a6e91ca3d3e27549bd2bf24b36e7f
vaa25 <alex.vlasov.ua@gmail.com>
2018-07-21 20:45:56 +0300

    reset id autoincrement in AbstractDbunitTest.java

--- src/test/java/com/provectus/vaa25/AbstractDbunitTest.java
+++ src/test/java/com/provectus/vaa25/AbstractDbunitTest.java
@@ -1,8 +1,11 @@
 package com.provectus.vaa25;
 
 import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
@@ -11,5 +14,16 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 @SpringBootTest
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class, TransactionDbUnitTestExecutionListener.class})
 public abstract class AbstractDbunitTest {
+
+    @Autowired
+    private JdbcTemplate template;
+
+    @Before
+    public void setUp() {
+        this.template.update("ALTER TABLE author ALTER COLUMN id RESTART WITH 1");
+        this.template.update("ALTER TABLE genre ALTER COLUMN id RESTART WITH 1");
+        this.template.update("ALTER TABLE book ALTER COLUMN id RESTART WITH 1");
+        this.template.update("ALTER TABLE purchase ALTER COLUMN id RESTART WITH 1");
+    }
 
 }
